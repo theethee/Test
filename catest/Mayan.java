d5137d8e269cbf43dfcb6bd622e30156f8671464
Sena <70093768+Sena-AH@users.noreply.github.com>
2020-10-21 17:16:52 +0200

    add folder
    full folder added

---
+++ catest/Mayan.java
@@ -0,0 +1,15 @@
+package catest;
+
+public class Mayan extends Language{
+	  
+	   protected Mayan(String langName, int langNumSpeakers) {
+		super(langName, langNumSpeakers, "Central America", "verb-object-subject");
+	
+	}
+	   @Override
+	   public void getInfo(){
+
+	       System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionSpoken + ". \nThe language follows the word order: " + this.wordOrder + ".\nFun fact:" + this.name +" is an ergative language.");
+	     }
+	
+	}
---
+++ catest/SinoTibetan.java
@@ -0,0 +1,11 @@
+package catest;
+
+public class SinoTibetan extends Language{
+	  
+	  protected SinoTibetan(String langName, int langNumSpeakers){
+	    super(langName, langNumSpeakers, "Asia", "subject-object-verb");
+	    if (langName.contains("Chinese")) {
+	        this.wordOrder = "subject-verb-object";
+	    }
+	  }
+	}
