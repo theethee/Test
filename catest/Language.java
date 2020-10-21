0b7f71e9176b873b42ce50ffa490ff4ca9701ad4
Thitisuda Mahasrithanet <thiti.m90@gmail.com>
2020-10-21 18:01:50 +0200

    Revert "Added a special comment to Sena and deleted "HELLO WORLD AND HELLO SENA!!!""
    This reverts commit 246e03cd3ec4159c57b97a3f7be83d60f0e56457.

--- catest/Language.java
+++ catest/Language.java
@@ -3,7 +3,7 @@ package catest;
 public class Language{
  
   protected String name;
-  protected int numSpeakers; // BAJS!
+  protected int numSpeakers; // HELLO WORLD AND HELLO SENA!!!!
   protected String regionSpoken;
   protected String wordOrder;
 
